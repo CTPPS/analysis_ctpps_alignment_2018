5d9db252cab03149b1f61e3bd469f05de1a6aea5
Jan Kašpar <jan.kaspar@cern.ch>
2019-05-03 19:04:53 +0200

    Updated default reference.

--- x_alignment_meth_o.cc
+++ x_alignment_meth_o.cc
@@ -289,7 +289,7 @@ int main()
 			char buf[100];
 			unsigned int xangle_def = (cfg.xangle > 0) ? cfg.xangle : 160;
 			double beta_def = (cfg.beta > 0) ? cfg.beta : 0.3;
-			sprintf(buf, "data/alig/fill_6554/xangle_%u_beta_%.2f/DS1", xangle_def, beta_def);
+			sprintf(buf, "data/alig-version-old/fill_6554/xangle_%u_beta_%.2f/DS1", xangle_def, beta_def);
 			ref = buf;
 		}
 
