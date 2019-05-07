c7e2cd0270b5966813f9fa677326989a0020bccc
Jan Kašpar <jan.kaspar@cern.ch>
2019-05-07 11:10:50 +0200

    Sector 56 cuts tuned.

--- data/phys-version1/fill_6624/config_common.py
+++ data/phys-version1/fill_6624/config_common.py
@@ -7,3 +7,5 @@ sys.path.append(os.path.relpath("../../../../../"))
 from config_base import *
 
 ApplyDefaultSettings2()
+config.sector_56.cut_h_c = -39.26 - 0.35 + 0.11
+config.sector_56.cut_v_c = 1.49 - 0.5 + 0.98
