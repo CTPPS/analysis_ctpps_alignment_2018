98b7b6974545a0f7f0981286d8c06ec6306d4ccd
Jan Kašpar <jan.kaspar@cern.ch>
2019-07-08 17:31:38 +0200

    Added sh_x correction and plots of x slope.

--- aux_fits/make_fits.cc
+++ aux_fits/make_fits.cc
@@ -40,6 +40,36 @@ struct RPGraphs
 
 //----------------------------------------------------------------------------------------------------
 
+struct SectorGraphs
+{
+	TGraphErrors *g_x_slope = NULL;
+
+	TF1 *f_x_slope = NULL;
+
+	void Init()
+	{
+		if (g_x_slope)
+			return;
+
+		g_x_slope = new TGraphErrors();
+	}
+
+	void Fit()
+	{
+		f_x_slope = new TF1("", "[0]");
+		g_x_slope->Fit(f_x_slope, "Q");
+	}
+
+	void Write() const
+	{
+		g_x_slope->Write("g_x_slope");
+		if (f_x_slope)
+			f_x_slope->Write("f_x_slope");
+	}
+};
+
+//----------------------------------------------------------------------------------------------------
+
 int main()
 {
 	// initialisation
@@ -63,6 +93,11 @@ int main()
 		"R_2_F",
 	};
 
+	vector<string> sectors = {
+		"sector 45",
+		"sector 56",
+	};
+
 	// prepare output
 	TFile *f_out = TFile::Open("fits.root", "recreate");
 
@@ -73,6 +108,7 @@ int main()
 
 		// collect data (build fill-dependent graphs)
 		map<string, RPGraphs> rpGraphs;
+		map<string, SectorGraphs> sectorGraphs;
 
 		for (const auto &fill : fills)
 		{
@@ -88,6 +124,8 @@ int main()
 
 			TFile *f_in_x_sh = TFile::Open((dir + "/x_alignment_meth_o.root").c_str());
 			string ref_x_sh = "data_alig-version1_fill_6554_" + cfg + "_DS1";
+
+			TFile *f_in_x_rel = TFile::Open((dir + "/x_alignment_relative.root").c_str());
 
 			TFile *f_in_y_tilt = TFile::Open((dir + "/y_alignment.root").c_str());
 
@@ -109,8 +147,14 @@ int main()
 						const double x_sh = g_results->GetX()[0];
 						const double x_sh_unc = g_results->GetY()[0];
 
+						double corr = 0;
+						if (rp == "L_2_F") corr = -0.200;
+						if (rp == "L_1_F") corr = -0.250;
+						if (rp == "R_1_F") corr = +0.250;
+						if (rp == "R_2_F") corr = +0.200;
+
 						int idx = g.g_x_sh->GetN();
-						g.g_x_sh->SetPoint(idx, fill, x_sh);
+						g.g_x_sh->SetPoint(idx, fill, x_sh + corr);
 						g.g_x_sh->SetPointError(idx, 0., x_sh_unc);
 					}
 				}
@@ -134,6 +178,29 @@ int main()
 				}
 			}
 
+			for (const auto &sector : sectors)
+			{
+				auto &g = sectorGraphs[sector];
+				g.Init();
+
+				if (f_in_x_rel)
+				{
+					TGraph *g_results = (TGraph *) f_in_x_rel->Get((sector + "/g_results").c_str());
+
+					//printf("* %s, %p\n", rp.c_str(), g_results);
+
+					if (g_results)
+					{
+						const double slope = g_results->GetX()[1];
+						const double slope_unc = g_results->GetY()[1];
+
+						int idx = g.g_x_slope->GetN();
+						g.g_x_slope->SetPoint(idx, fill, slope);
+						g.g_x_slope->SetPointError(idx, 0., slope_unc);
+					}
+				}
+			}
+
 			if (f_in_x_sh)
 				delete f_in_x_sh;
 
@@ -141,20 +208,26 @@ int main()
 				delete f_in_y_tilt;
 		}
 
-		// fit graphs
-		for (auto &g : rpGraphs)
-		{
-			g.second.f_x_sh = new TF1("", "([0] + [1]*x) + (x > 6670) * ([2] + [3]*x) + (x > 6800) * ([4] + [5]*x) + (x > 6980) * ([6] + [7]*x)  + (x > 7180) * ([8] + [9]*x)");
-			g.second.g_x_sh->Fit(g.second.f_x_sh, "Q");
-
-			g.second.f_y_tilt = new TF1("", "[0] + (x > 6854) * ([1]) + (x > 7213) * ([2])");
-			g.second.g_y_tilt->Fit(g.second.f_y_tilt, "Q");
-		}
-
-		// save fits
+		// fit and save graphs
 		for (auto &p : rpGraphs)
 		{
 			gDirectory = d_cfg->mkdir(p.first.c_str());
+
+			p.second.f_x_sh = new TF1("", "([0] + [1]*x) + (x > 6670) * ([2] + [3]*x) + (x > 6800) * ([4] + [5]*x) + (x > 6980) * ([6] + [7]*x)  + (x > 7180) * ([8] + [9]*x)");
+			p.second.g_x_sh->Fit(p.second.f_x_sh, "Q");
+
+			p.second.f_y_tilt = new TF1("", "[0] + (x > 6854) * ([1]) + (x > 7213) * ([2])");
+			p.second.g_y_tilt->Fit(p.second.f_y_tilt, "Q");
+
+			p.second.Write();
+		}
+
+		for (auto &p : sectorGraphs)
+		{
+			gDirectory = d_cfg->mkdir(p.first.c_str());
+
+			p.second.Fit();
+
 			p.second.Write();
 		}
 	}
